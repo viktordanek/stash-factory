48ac85b54bdf74e458a2ffeb2ff2cacd311abdc5
Viktor Danek <viktordanek10@gmail.com>
2025-05-15 12:59:12 -0400

    is this necessary

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
                                         pkgs.writeShellApplication
                                             {
                                                 name = "generic generator" ;
-                                                runtimeImports = [ pkgs.coreutils pkgs.findutils pkgs.flock ] ;
+                                                runtimeInputs = [ pkgs.coreutils pkgs.findutils pkgs.flock ] ;
                                                 text =
                                                     let
                                                         input-hash = builtins.hashString "sha512" ( builtins.toJSON primary ) ;
