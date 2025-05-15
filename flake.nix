53b0a2370d6a5f9535fcd8d5e02c742f0c3e214f
Viktor Danek <viktordanek10@gmail.com>
2025-05-15 12:16:45 -0400

    is this necessary

--- flake.nix
+++ flake.nix
@@ -16,55 +16,57 @@
                             stash-directory ,
                             time-mask
                         } @primary :
-                            pkgs.writeShellApplication
-                                {
-                                    name = "generic generator" ;
-                                    runtimeImports = [ pkgs.coreutils pkgs.findutils pkgs.flock ] ;
-                                    text =
-                                        let
-                                            input-hash = builtins.hashString "sha512" ( builtins.toJSON primary ) ;
-                                            in
-                                                ''
-                                                    set -e
-                                                    TIMESTAMP="$( date "${ time-mask }" )"
-                                                    HASH="$( printf "%s%s" "${ input-hash }" "$TIMESTAMP" | sha512sum | cut -c1-${ builtins.toString hash-length } )"
-                                                    MOUNT="${ stash-directory }/$HASH"
-                                                    mkdir --parents "${ stash-directory }"
-                                                    LOCK="$MOUNT.lock"
-                                                    exec 201> "$LOCK"
-                                                    flock -x 201
-                                                    cleanup ( ) {
-                                                        flock -u 201
-                                                        rm --force "$LOCK"
-                                                    }
-                                                    trap cleanup EXIT
-                                                    TARGET="$MOUNT/target"
-                                                    if [ -d "$MOUNT" ]
-                                                    then
-                                                        printf '%s\n' "$TARGET"
-                                                        exit 0
-                                                    else
-                                                        if [ -e "$MOUNT" ]
-                                                        then
-                                                            mv "$MOUNT" "$MOUNT.trash.$( date +%s )"
-                                                        fi
-                                                        mkdir --parents "$MOUNT"
-                                                        if ${ generator.generate generation-parameters } "$TARGET" > "$MOUNT/standard-output" 2> "$MOUNT/standard-error"
-                                                        then
-                                                            STATUS=$?
-                                                            echo "$STATUS" > "$MOUNT/status"
-                                                            printf '%s\n' "$TARGET"
-                                                            exit 0
-                                                        else
-                                                            STATUS=$?
-                                                            echo "$STATUS" > "$MOUNT/status"
-                                                            mv "$MOUNT" "$MOUNT.failed.$( date +%s)"
-                                                            exit "$STATUS"
-                                                        fi
-                                                    fi
-                                                '' ;
-                            } ;
+                            let
+                                pkgs = import nixpkgs { inherit system ; } ;
+                                in
+                                    pkgs.writeShellApplication
+                                        {
+                                            name = "generic generator" ;
+                                            runtimeImports = [ pkgs.coreutils pkgs.findutils pkgs.flock ] ;
+                                            text =
+                                                let
+                                                    input-hash = builtins.hashString "sha512" ( builtins.toJSON primary ) ;
+                                                    in
+                                                        ''
+                                                            set -e
+                                                            TIMESTAMP="$( date "${ time-mask }" )"
+                                                            HASH="$( printf "%s%s" "${ input-hash }" "$TIMESTAMP" | sha512sum | cut -c1-${ builtins.toString hash-length } )"
+                                                            MOUNT="${ stash-directory }/$HASH"
+                                                            mkdir --parents "${ stash-directory }"
+                                                            LOCK="$MOUNT.lock"
+                                                            exec 201> "$LOCK"
+                                                            flock -x 201
+                                                            cleanup ( ) {
+                                                                flock -u 201
+                                                                rm --force "$LOCK"
+                                                            }
+                                                            trap cleanup EXIT
+                                                            TARGET="$MOUNT/target"
+                                                            if [ -d "$MOUNT" ]
+                                                            then
+                                                                printf '%s\n' "$TARGET"
+                                                                exit 0
+                                                            else
+                                                                if [ -e "$MOUNT" ]
+                                                                then
+                                                                    mv "$MOUNT" "$MOUNT.trash.$( date +%s )"
+                                                                fi
+                                                                mkdir --parents "$MOUNT"
+                                                                if ${ generator.generate generation-parameters } "$TARGET" > "$MOUNT/standard-output" 2> "$MOUNT/standard-error"
+                                                                then
+                                                                    STATUS=$?
+                                                                    echo "$STATUS" > "$MOUNT/status"
+                                                                    printf '%s\n' "$TARGET"
+                                                                    exit 0
+                                                                else
+                                                                    STATUS=$?
+                                                                    echo "$STATUS" > "$MOUNT/status"
+                                                                    mv "$MOUNT" "$MOUNT.failed.$( date +%s)"
+                                                                    exit "$STATUS"
+                                                                fi
+                                                            fi
+                                                        '' ;
+                                    } ;
                 } ;
-            pkgs = import nixpkgs { inherit system ; } ;
             in flake-utils.lib.eachDefaultSystem fun ;
 }
