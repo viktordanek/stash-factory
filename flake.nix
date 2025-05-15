c39e69bea2223df5683c26111b3ee146b3426e5c
Viktor Danek <viktordanek10@gmail.com>
2025-05-15 14:31:42 -0400

    is this necessary

--- flake.nix
+++ flake.nix
@@ -44,7 +44,7 @@
                                                                 LOCK="$MOUNT.lock"
                                                                 exec 201> "$LOCK"
                                                                 flock -x 201
-                                                                # spellcheck disable=SC2317
+                                                                # shellcheck disable=SC2317
                                                                 cleanup ( ) {
                                                                     flock -u 201
                                                                     rm --force "$LOCK"
